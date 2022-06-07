825135726a01d7497d4ac0c0e7621805e282f5c8
majharul <majharuljony163@gmail.com>
2022-06-07 23:07:04 +0600

    fetch data app using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterView from "./features/counter/CounterView";
+import PostView from "./features/post/postView";
 
 function App() {
-  return (
-    <div className="App">
-     <h3>Counter App </h3>
-      <CounterView/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h3>Counter App </h3>
+            <CounterView/>
+            <PostView/>
+        </div>
+    );
 }
 
 export default App;
